f8af7e2c3ce92c8f5ec1cc262d79b36120636e8c
Hiro Kai <fullerenec84@gmail.com>
2014-07-15 11:29:44 -0700

    Progress bar

--- multitimer/js/main.js
+++ multitimer/js/main.js
@@ -6,6 +6,8 @@ TimerApp.controller('EntireCtrl', function ($scope) {
 		return {name: ''+(i+1),value: 0};
 	});
 
+	$scope.fullTime = 5;
+
 	$scope.addTimer = function(){
 		$scope.timers.push({name: mkName(),value:0});
 	};
@@ -46,6 +48,35 @@ TimerApp.controller('EntireCtrl', function ($scope) {
 	};
 });
 
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function rgbToStr(c){
+	return '#' + _.map(c,function(e){return e.toString(16);}).join('');
+};
+
 TimerApp.controller('TimerCtrl',function ($scope,$interval) {
 	$scope.running = false;
 	$scope.selected = false;
@@ -54,8 +85,13 @@ TimerApp.controller('TimerCtrl',function ($scope,$interval) {
 
 		$scope.startTime = new Date().getTime();
 		$scope.running = true;
+		var factor = 1;
 		$scope.timerObj = $interval(function(){
-			$scope.timer.value = (new Date().getTime() - $scope.startTime)/1000;
+			$scope.timer.value = (new Date().getTime() - $scope.startTime)/1000*factor;
+			$scope.barWidth = ""+Math.min($scope.timer.value / ($scope.fullTime*60) * 100,123)+"px";
+			var r = Math.min($scope.timer.value / ($scope.fullTime*60),1);
+		//	$scope.barColor = r < 0.5 ? 'lightblue' : (r < 0.8 ? 'orange' : 'red');
+			$scope.barColor = rgbToStr(hslToRgb(0.5*(1-r),0.5,0.5));
 		},200);
 	};
 	$scope.timerStr = function(){

4913823cef16473109c288862addf0de609d369e
Hiro Kai <fullerenec84@gmail.com>
2014-12-23 21:11:36 -0800

    JRECIN data

--- jrecin/js/services.js
+++ jrecin/js/services.js
@@ -11,7 +11,7 @@ function formatDate(d){
 
 phonecatServices.factory('Phone', ['$resource',
   function($resource){
-	var data_id = '20141219';
+	var data_id = '20141223';
     return $resource('data/'+data_id+'/:phoneId.json', {}, {
       query: {method:'GET', params:{phoneId:'list'}, isArray:true,
       			transformResponse: function(str){

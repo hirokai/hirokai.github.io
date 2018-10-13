51fd08473ac3c66de55c38d18d393b56b282aeab
Hiro Kai <fullerenec84@gmail.com>
2018-10-13 14:08:27 +0200

    Update format for publication

--- about/assemble/src/helpers/helper.js
+++ about/assemble/src/helpers/helper.js
@@ -1,17 +1,17 @@
 module.exports.paper = function (paper)  {
 	var res = "";
 	if(paper.authors){
-		res += paper.authors + "; "
-		res += "&quot;" + paper.title + "&quot; "
-		res += paper.journal_cit + ".";
+		res += paper.authors + "; <br/>"
+		res += "&quot;" + paper.title + "&quot<br/>"
+		res += "<a href='"+paper.journal+"'>" + paper.journal_cit + ".</a>";
 	}else{
 		res += paper.citation;
-	}
-	if(paper.pubmed){
-		res += '&nbsp;[PubMed ID: <a href="http://www.ncbi.nlm.nih.gov/pubmed/'+paper.pubmed+'">'+paper.pubmed+'</a>]';
-	}
-	if(paper.journal){
-		res += '&nbsp;[<a href="'+paper.journal+'">Journal</a>]';
+		if(paper.pubmed){
+			res += '&nbsp;[PubMed ID: <a href="http://www.ncbi.nlm.nih.gov/pubmed/'+paper.pubmed+'">'+paper.pubmed+'</a>]';
+		}
+		if(paper.journal){
+			res += '&nbsp;[<a href="'+paper.journal+'">Journal</a>]';
+		}
 	}
 	return res;
 };

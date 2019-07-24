4560777f2519193da14d1c6960ccf72301c9f2ee
Hiro Kai <fullerenec84@gmail.com>
2019-07-24 22:30:01 +0900

    Add presentation schedule. Fix presentation.en

--- about/assemble/src/helpers/helper.js
+++ about/assemble/src/helpers/helper.js
@@ -1,29 +1,57 @@
-module.exports.paper = function (paper)  {
+module.exports.paper = function (paper) {
 	var res = "";
-	if(paper.authors){
+	if (paper.authors) {
 		res += paper.authors + "; <br/>"
 		res += "&quot;" + paper.title + "&quot<br/>"
-		res += "<a href='"+paper.journal+"'>" + paper.journal_cit + ".</a>";
-	}else{
+		res += "<a href='" + paper.journal + "'>" + paper.journal_cit + ".</a>";
+	} else {
 		res += paper.citation;
-		if(paper.pubmed){
-			res += '&nbsp;[PubMed ID: <a href="http://www.ncbi.nlm.nih.gov/pubmed/'+paper.pubmed+'">'+paper.pubmed+'</a>]';
+		if (paper.pubmed) {
+			res += '&nbsp;[PubMed ID: <a href="http://www.ncbi.nlm.nih.gov/pubmed/' + paper.pubmed + '">' + paper.pubmed + '</a>]';
 		}
-		if(paper.journal){
-			res += '&nbsp;[<a href="'+paper.journal+'">Journal</a>]';
+		if (paper.journal) {
+			res += '&nbsp;[<a href="' + paper.journal + '">Journal</a>]';
 		}
 	}
 	return res;
 };
 
-module.exports.presentation = function (p)  {
-	var res = "<li data-kind='"+(p.kind || '')+"'>";
-	if(p.authors){
+module.exports.presentation = function (p) {
+	var res = "<li data-kind='" + (p.kind || '') + "'>";
+	if (p.authors) {
 		res += "<span class='authors'>" + p.authors + "<br/></span>";
 		res += "<span class='title'>" + p.title + (p.invited ? "<strong>（招待講演）</strong>" : "") + "<br/></span>";
-		res += "<span class='conference'>" + (p.url ? ("<a href='"+p.url+"'>"+p.conference+"</a>") : p.conference);
+		res += "<span class='conference'>" + (p.url ? ("<a href='" + p.url + "'>" + p.conference + "</a>") : p.conference);
 		res += " (" + p.date + "; " + p.place + ")<br/></span>";
-	}else{
+	} else {
+		res += p;
+	}
+	res += '</li>'
+	return res;
+};
+
+module.exports.presentation_en = function (p) {
+	var res = "<li data-kind='" + (p.kind || '') + "'>";
+	if (p.authors) {
+		res += "<span class='authors'>" + p.authors + "<br/></span>";
+		res += "<span class='title'>" + p.title + (p.invited ? "<strong>（招待講演）</strong>" : "") + "<br/></span>";
+		res += "<span class='conference'>" + (p.url ? ("<a href='" + p.url + "'>" + p.conference + "</a>") : p.conference);
+		res += " (" + p.date + "; " + p.place + ")<br/></span>";
+	} else {
+		res += p;
+	}
+	res += '</li>'
+	return res;
+};
+
+module.exports.presentation_future_ja = function (p) {
+	const kind_name = { poster: "ポスター発表", oral: "口頭発表" }
+	var res = "<li data-kind='" + (p.kind || '') + "'>";
+	if (p.authors) {
+		res += p.date + " " + p.time + " <a href='" + p.url + "'>" + p.conference + "</a>（" + p.place + "）<br>";
+		res += p.title + "（" + (p.special_kind || kind_name[p.kind]) + "）" + (p.program_url ? ("<a href=" + p.program_url + ">[プログラム]</a>") : "") + "<br>";
+		res += p.authors + "</li>"
+	} else {
 		res += p;
 	}
 	res += '</li>'

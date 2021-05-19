2381336ef7fe676598a52d29f604c660893ca2c9
Hiro Kai <fullerenec84@gmail.com>
2021-05-19 12:14:55 +0900

    JSON and JS formatter.

--- about/assemble/src/helpers/helper.js
+++ about/assemble/src/helpers/helper.js
@@ -1,60 +1,60 @@
 module.exports.paper = function (paper) {
-	var res = "";
-	if (paper.authors) {
-		res += paper.authors + "; <br/>"
-		res += "&quot;" + paper.title + "&quot<br/>"
-		res += "<a href='" + paper.journal + "'>" + paper.journal_cit + ".</a>";
-	} else {
-		res += paper.citation;
-		if (paper.pubmed) {
-			res += '&nbsp;[PubMed ID: <a href="http://www.ncbi.nlm.nih.gov/pubmed/' + paper.pubmed + '">' + paper.pubmed + '</a>]';
-		}
-		if (paper.journal) {
-			res += '&nbsp;[<a href="' + paper.journal + '">Journal</a>]';
-		}
-	}
-	return res;
-};
+  let res = ''
+  if (paper.authors) {
+    res += paper.authors + '; <br/>'
+    res += '&quot;' + paper.title + '&quot<br/>'
+    res += "<a href='" + paper.journal + "'>" + paper.journal_cit + '.</a>'
+  } else {
+    res += paper.citation
+    if (paper.pubmed) {
+      res += '&nbsp;[PubMed ID: <a href="http://www.ncbi.nlm.nih.gov/pubmed/' + paper.pubmed + '">' + paper.pubmed + '</a>]'
+    }
+    if (paper.journal) {
+      res += '&nbsp;[<a href="' + paper.journal + '">Journal</a>]'
+    }
+  }
+  return res
+}
 
 module.exports.presentation = function (p) {
-	var res = "<li data-kind='" + (p.kind || '') + "'>";
-	if (p.authors) {
-		res += "<span class='authors'>" + p.authors + "<br/></span>";
-		res += "<span class='title'>" + p.title + (p.special_kind ? "<strong>（" + p.special_kind + "）</strong>" : p.invited ? "<strong>（招待講演）</strong>" : "") + "<br/></span>";
-		res += "<span class='conference'>" + (p.url ? ("<a href='" + p.url + "'>" + p.conference + "</a>") : p.conference);
-		res += " (" + p.date + "; " + p.place + ")<br/></span>";
-	} else {
-		res += p;
-	}
-	res += '</li>'
-	return res;
-};
+  let res = "<li data-kind='" + (p.kind || '') + "'>"
+  if (p.authors) {
+    res += "<span class='authors'>" + p.authors + '<br/></span>'
+    res += "<span class='title'>" + p.title + (p.special_kind ? '<strong>（' + p.special_kind + '）</strong>' : p.invited ? '<strong>（招待講演）</strong>' : '') + '<br/></span>'
+    res += "<span class='conference'>" + (p.url ? ("<a href='" + p.url + "'>" + p.conference + '</a>') : p.conference)
+    res += ' (' + p.date + '; ' + p.place + ')<br/></span>'
+  } else {
+    res += p
+  }
+  res += '</li>'
+  return res
+}
 
 module.exports.presentation_en = function (p) {
-	var res = "<li data-kind='" + (p.kind || '') + "'>";
-	if (p.authors) {
-		res += "<span class='authors'>" + p.authors + "<br/></span>";
-		res += "<span class='title'>" + p.title + (p.special_kind ? "<strong>（" + p.special_kind + "）</strong>" : p.invited ? "<strong>（invited）</strong>" : "") + "<br/></span>";
+  let res = "<li data-kind='" + (p.kind || '') + "'>"
+  if (p.authors) {
+    res += "<span class='authors'>" + p.authors + '<br/></span>'
+    res += "<span class='title'>" + p.title + (p.special_kind ? '<strong>（' + p.special_kind + '）</strong>' : p.invited ? '<strong>（invited）</strong>' : '') + '<br/></span>'
 
-		res += "<span class='conference'>" + (p.url ? ("<a href='" + p.url + "'>" + p.conference + "</a>") : p.conference);
-		res += " (" + p.date + "; " + p.place + ")<br/></span>";
-	} else {
-		res += p;
-	}
-	res += '</li>'
-	return res;
-};
+    res += "<span class='conference'>" + (p.url ? ("<a href='" + p.url + "'>" + p.conference + '</a>') : p.conference)
+    res += ' (' + p.date + '; ' + p.place + ')<br/></span>'
+  } else {
+    res += p
+  }
+  res += '</li>'
+  return res
+}
 
 module.exports.presentation_future_ja = function (p) {
-	const kind_name = { poster: "ポスター発表", oral: "口頭発表" }
-	var res = "<li data-kind='" + (p.kind || '') + "'>";
-	if (p.authors) {
-		res += p.date + " " + p.time + " <a href='" + p.url + "'>" + p.conference + "</a>（" + p.place + "）<br>";
-		res += p.title + "（" + (p.special_kind || kind_name[p.kind]) + "）" + (p.program_url ? ("<a href=" + p.program_url + ">[プログラム]</a>") : "") + "<br>";
-		res += p.authors + "</li>"
-	} else {
-		res += p;
-	}
-	res += '</li>'
-	return res;
-};
+  const kind_name = { poster: 'ポスター発表', oral: '口頭発表' }
+  let res = "<li data-kind='" + (p.kind || '') + "'>"
+  if (p.authors) {
+    res += p.date + ' ' + p.time + " <a href='" + p.url + "'>" + p.conference + '</a>（' + p.place + '）<br>'
+    res += p.title + '（' + (p.special_kind || kind_name[p.kind]) + '）' + (p.program_url ? ('<a href=' + p.program_url + '>[プログラム]</a>') : '') + '<br>'
+    res += p.authors + '</li>'
+  } else {
+    res += p
+  }
+  res += '</li>'
+  return res
+}
